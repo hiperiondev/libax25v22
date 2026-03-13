2c6c8e496126587c05db5b255bdaa514bbc7b2b1
hiperiondev <egonzalez.hiperion@gmail.com>
2026-03-12 22:08:30 -0300

    Correct linux test

---
+++ temp/ax25_linux_test.h
@@ -0,0 +1,17 @@
+/*
+ * ax25_linux_test.h
+ *
+ *  Created on: 11 mar 2026
+ *      Author: egonzalez
+ */
+
+#ifndef AX25_LINUX_TEST_H_
+#define AX25_LINUX_TEST_H_
+
+/**
+ * @brief Run the complete libax25v22 test suite.
+ * @return 0 if all tests pass, 1 if any test fails.
+ */
+int linux_test_main(void);
+
+#endif /* AX25_LINUX_TEST_H_ */
